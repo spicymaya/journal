e83d78dec867fdfb277f5918cbd9613d354d79a6
Maya Davis <kryvitskaya@gmail.com>
2013-05-06 11:01:45 +0300

    Post
    Submission, listing
    Text truncate

--- journal.rb
+++ journal.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require 'slim'
 require 'sass'
+require 'data_mapper'
+
+DataMapper.setup(:default, 'mysql://root:@127.0.0.1/journal')
+class Post
+	include DataMapper::Resource
+	property :id, Serial
+	property :title, String
+	property :date, Date
+	property :body, Text #stores more text (serial, string, date, text are database types)
+end
+DataMapper.finalize #just need to do it
+DataMapper.auto_upgrade! #create and change the db automatically
 
 get '/style.css' do
  scss(:style)
@@ -13,8 +25,21 @@ get '/about' do
 end
 get '/info' do
 	slim(:info)
-end		
-get '/form' do
+end	
+get '/posts' do
+	@posts = Post.all
+	slim(:posts)
+end
+
+get '/posts/new' do
 	slim(:form)
 end
+post '/posts' do
+	# params.inspect #params- object which is a hash
+	# params["title"] access individual keys/values
+	post = Post.new(params) #creates post in memory
+	post.save #writes post to the db
+	redirect '/posts'
+end 	
+	 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){	
 	
 	$('.datepicker').datepicker();
-	
+	$(document).keyup(function(e) {
+  	if (e.keyCode == 27) { // esc
+  		$('.datepicker').datepicker('hide'); 
+		}   
+	});
+	$('.datepicker').on('changeDate', function() {
+		$('.datepicker').datepicker('hide'); 
+	})
 });
