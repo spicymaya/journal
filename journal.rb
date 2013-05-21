dcbce8beb5e0a8dea125c49b82e713f7e5c11f97
Maya Davis <kryvitskaya@gmail.com>
2013-05-21 13:41:13 -0400

    Design change+edit feature
    JS triple return key

--- journal.rb
+++ journal.rb
@@ -2,29 +2,37 @@ require 'sinatra'
 require 'slim'
 require 'sass'
 require 'data_mapper'
+require 'compass'
 
-DataMapper.setup(:default, 'mysql://root:@127.0.0.1/journal')
-class Post
+
+configure do
+	Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
+end	
+DataMapper.setup(:default, 'mysql://root:@127.0.0.1/journal') #find on the website
+class Post #model
 	include DataMapper::Resource
-	property :id, Serial
-	property :title, String
-	property :date, Date
-	property :body, Text #stores more text (serial, string, date, text are database types)
+	property(:id, Serial)
+	property(:title, String)
+	property(:date, Date)
+	property(:body, Text) #stores more text (serial, string, date, text are database types)
 end
 DataMapper.finalize #just need to do it
 DataMapper.auto_upgrade! #create and change the db automatically
 
+#doing the job of controllers; .slim files are views
 get '/style.css' do
  scss(:style)
 end
-get '/hi' do
-  "Hello World!"
+
+get '/' do
+	@body_class = "home"
+ 	slim(:index)
 end
 get '/about' do
 	slim(:about)
 end
-get '/info' do
-	slim(:info)
+get '/pics' do
+	slim(:pics)
 end	
 get '/posts' do
 	@posts = Post.all
@@ -32,14 +40,44 @@ get '/posts' do
 end
 
 get '/posts/new' do
+	protected!
+	@post=Post.new
 	slim(:form)
 end
+get '/posts/:id/edit' do
+	protected!
+	@post = Post.get(params[:id])
+	@body_class="edit"
+	slim(:form)
+end
+
 post '/posts' do
 	# params.inspect #params- object which is a hash
 	# params["title"] access individual keys/values
 	post = Post.new(params) #creates post in memory
 	post.save #writes post to the db
-	redirect '/posts'
-end 	
-	 
+	redirect to ('/posts')
+end
+get '/posts/:id' do # : because var in the route
+	#instant variable
+	@post = Post.get(params[:id]) #starts with a ":" because it's a symbol type of var, to_i converts a string to an integer
+	slim(:detail)
+end
+helpers do
+	def protected!
+		#password applies on the internet, but not on the computer (ENV-environment)
+	    if ENV['RACK_ENV'] == 'production'
+	      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+	      unless @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ["MAYA", "123"]
+	        response['WWW-Authenticate'] = %(Basic realm='Administration')
+	        throw(:halt, [401, "Not authorized\n"])
+	      end
+	    # end
+	  end
+end	
 
+delete '/posts/:id' do
+	Post.get(params[:id]).destroy
+	redirect to ('/posts')
+end
+end
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){	
-	
+var last_key_pressed;	
+var dlp=document.location.pathname;
 	$('.datepicker').datepicker();
 	$(document).keyup(function(e) {
   	if (e.keyCode == 27) { // esc
   		$('.datepicker').datepicker('hide'); 
-		}   
+		}  else if (e.keyCode == 13 && last_key_pressed==13) {
+				var dlp_split=dlp.split("/");
+				if dlp.indexOf('/posts') >= 0 && dlp.indexOf('/edit') < 0
+            post_id = $('.post').attr('id').replace('post_', '')
+            post_id=encodeURIComponent(post_id);
+            var string_url = "/posts/" + post_id + "/edit";
+            window.location = string_url;
+				
+
+		} 
+		last_key_pressed=e.keyCode;	
 	});
 	$('.datepicker').on('changeDate', function() {
 		$('.datepicker').datepicker('hide'); 
 	})
+	
 });
